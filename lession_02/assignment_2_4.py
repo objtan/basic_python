81b034e936bed192cbb6271ccec7139fb2955432
Objtan <objtan@gmail.com>
2022-07-06 10:36:34 +0900

    upda code assignment 2_4

--- lession_02/assignment_2_4.py
+++ lession_02/assignment_2_4.py
@@ -3,3 +3,8 @@ response = int(input('enter the number: '))
 while (i <= response):
     print (f'Hello world {i}')
     i += 1
+
+
+# response = int (input('enter: '))
+# for i in range (1,response):
+#     print (f'Hello world {i}')
