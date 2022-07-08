15e97fecbf4b7bbdd0ad5ff76a3154ab73cde481
Objtan <objtan@gmail.com>
2022-07-08 14:09:54 +0900

    update code assignment 2_1

--- lession_03/assignment_2_1.py
+++ lession_03/assignment_2_1.py
@@ -1,23 +1,30 @@
 # Create a variable to control the loop
-#keep_going = 'y'
+keep_going = 'y'
 
 # Create a "While" loop for the user wants to do another action
-#while keep_going == 'y':
+while keep_going == 'y':
     # ---------------------------------------------
     # ---------------------------------------------
     # Program code assignment 2_1
     # ---------------------------------------------]
     # ---------------------------------------------
     # Create a variable start with 0
-    #count = 1
+    count = 0
 
     # Enter a string
-my_string = input('Enter s string: ')
+    my_string = input('Enter s string: ')
+    
+    # Convert the string you just entered to lowercase
+    my_string_lower = (my_string.lower())
 
-my_string_lower = (my_string.lower())
+    # Use loop to count
+    for substring in my_string_lower:
+        if substring == 't':   # If not convert the string (line 18,19) then use code " if substring == 'T' or substring =='t'"
+            count += 1
+    print (f'Number of times the letter "T" or "t" appears in the string: {count}')
 
-print(my_string_lower)
-print(my_string.lower())
+
+    #print(my_string_lower)
 
     # See if the user wants to do another action
-    #keep_going = input('Do you want to perform another action? (Enter y for yes): ')
+    keep_going = input('Do you want to perform another action? (Enter y for yes): ')
