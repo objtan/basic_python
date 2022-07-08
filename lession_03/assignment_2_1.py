ae6b53b38d4a1d65c4b2d55b298bbb1d90d7d113
Objtan <objtan@gmail.com>
2022-07-08 13:31:51 +0900

    rename file assignment 2_1

---
+++ lession_03/assignment_2_1.py
@@ -0,0 +1,23 @@
+# Create a variable to control the loop
+#keep_going = 'y'
+
+# Create a "While" loop for the user wants to do another action
+#while keep_going == 'y':
+    # ---------------------------------------------
+    # ---------------------------------------------
+    # Program code assignment 2_1
+    # ---------------------------------------------]
+    # ---------------------------------------------
+    # Create a variable start with 0
+    #count = 1
+
+    # Enter a string
+my_string = input('Enter s string: ')
+
+my_string_lower = (my_string.lower())
+
+print(my_string_lower)
+print(my_string.lower())
+
+    # See if the user wants to do another action
+    #keep_going = input('Do you want to perform another action? (Enter y for yes): ')
